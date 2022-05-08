32cbe0dee977d4c0026ba2549a5dfd1e13abeb75
LucasFelicianoAlmeida <lucasfeliciano326@gmail.com>
2022-05-08 14:00:42 -0300

    sixth challenge

--- angular-challenges/src/app/components/star-ratings/star-ratings.component.ts
+++ angular-challenges/src/app/components/star-ratings/star-ratings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-star-ratings',
@@ -6,9 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./star-ratings.component.scss']
 })
 export class StarRatingsComponent implements OnInit {
-
+  @Input() public starIcon: any
+  @Input() public halfStar: any
+  @Input() public rating = 5
   constructor() { }
 
+  public get fullStars(): number[] {
+    var totalFullStars = Math.floor(this.rating)
+    if(totalFullStars > 5)
+    {
+      totalFullStars = 5
+    }
+    return Array(totalFullStars).fill(0)
+  }
+
+  public get hasHalfStar(): boolean {
+    const hasHalfStar = (this.rating - Math.floor(this.rating) >= 0.5) && this.rating !== 5
+    return hasHalfStar;
+  }
+
+  public get emptyStars(): number[] {
+    const highestRating = 5
+    const totalEmptyStars = Math.floor(highestRating - this.rating)
+    console.log(totalEmptyStars)
+    return Array(totalEmptyStars).fill(0);
+  }
   ngOnInit(): void {
   }
 
